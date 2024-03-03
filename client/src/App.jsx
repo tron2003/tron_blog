3ce8ac456343cc69f3ad40351c0c213c897b2de3
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-03 12:16:18 +0530

    added all necessasry dependenceries and pages and compents made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import dashboard from "./pages/dashboard";
+import about from "./pages/about";
+import profile from "./pages/profile";
+import signin from "./pages/signin";
+import signup from "./pages/signup";
+import home from "./pages/home";
+import project from "./pages/project";
 
 function App() {
-  
-
   return (
-    <>
-    <h1 className="text-3xl text-red-500">
+    <BrowserRouter>
+      <Routes>
+        <Route>
+<Route path="/" element={<home/>}/>
+<Route path="/sign-in" element={<signin/>}/>
+<Route path="/sign-up" element={<signup/>}/>
 
-      Kanish
-    </h1>
-    </>
-  )
+
+<Route path="/dashboard" element={<dashboard/>}/>
+<Route path="/about" element={<about/>}/>
+<Route path="/profile" element={<profile/>}/>
+<Route path="/projects" element={<project/>}/>
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

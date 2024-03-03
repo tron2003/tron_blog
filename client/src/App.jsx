7f47ed52a4231bad0bf5d657349d02d6170ac1ad
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-03 12:16:41 +0530

    added dependries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,30 +3,27 @@ import { useState } from "react";
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import dashboard from "./pages/dashboard";
-import about from "./pages/about";
-import profile from "./pages/profile";
-import signin from "./pages/signin";
-import signup from "./pages/signup";
-import home from "./pages/home";
-import project from "./pages/project";
+import Dashboard from "./pages/dashboard";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Home from "./pages/home";
+import Project from "./pages/project";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
-<Route path="/" element={<home/>}/>
-<Route path="/sign-in" element={<signin/>}/>
-<Route path="/sign-up" element={<signup/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
 
-
-<Route path="/dashboard" element={<dashboard/>}/>
-<Route path="/about" element={<about/>}/>
-<Route path="/profile" element={<profile/>}/>
-<Route path="/projects" element={<project/>}/>
-
-
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/projects" element={<Project />} />
         </Route>
       </Routes>
     </BrowserRouter>

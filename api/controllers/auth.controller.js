ad34e4e32be8d04710597c8e0a525cfcd4996685
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-06 18:31:47 +0530

    error handler path in if statement

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,11 +5,9 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password || username === "" || email === "") {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  {
     next(errorHandler(400, "All fields are required"));
   }
+  
 
   // var salt = bcrypt.genSaltSync(10);
 

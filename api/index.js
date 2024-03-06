25a84ac0e60cccabf5337656ddd75a4d922bcbba
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-06 18:29:55 +0530

    error handler for error

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import userRoutes from "./routes/user.route.js"
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 const app = express();
 
 dotenv.config();
-app.use(express.json())
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -19,5 +19,15 @@ mongoose
 app.listen(3000, () => {
   console.log("Port running on 3000 !");
 });
-app.use("/api/user",userRoutes)
-app.use("/api/auth",authRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statuscode || 500;
+  const message = err.message || "internal server error";
+  res.status(statusCode).json({
+    sucess: false,
+    statusCode,
+    message,
+  });
+});

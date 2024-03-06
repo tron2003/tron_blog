ac0182df59e5aab396760fc596060698e668bb0e
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-06 16:01:24 +0530

    added mongodb and dotenv

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+const app = express();
 
+dotenv.config();
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-app.listen(3000,()=>{
-
-
-    console.log("Port running on 3000 !")
-})
-
-
+app.listen(3000, () => {
+  console.log("Port running on 3000 !");
+});

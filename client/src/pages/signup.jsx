d575f6971d7545bb2e9440676507c17ddfb5635b
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-08 00:01:46 +0530

    sing-up api and frontend

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Alert,
+  Button,
+  Label,
+  RangeSlider,
+  Spinner,
+  TextInput,
+} from "flowbite-react";
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 export default function signup() {
+  const navigate = useNavigate()
+  const [formdata, setformdata] = useState({});
+  const [errordata, seterrordata] = useState(null);
+  const [succesdata, setsuccesdata] = useState(null);
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    if (!formdata.username || !formdata.email || !formdata.password) {
+      return seterrordata("please fill out all details");
+    }
+    try {
+      setsuccesdata(true);
+      seterrordata(null)
+      const res = await fetch(`/api/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return seterrordata(data.message);
+      }
+      setsuccesdata(false);
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      seterrordata(error.message);
+      setsuccesdata(false);
+    }
+  };
+  const handleChange = (e) => {
+    setformdata({ ...formdata, [e.target.id]: e.target.value.trim() });
+  };
+  console.log(formdata);
   return (
     <div className=" min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-7">
@@ -18,33 +59,65 @@ export default function signup() {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-3">
+          <form className="flex flex-col gap-3" onSubmit={handlesubmit}>
             <div>
               <Label value="UserName" />
 
-              <TextInput type="text" placeholder="UserName" id="username" />
+              <TextInput
+                type="username"
+                placeholder="UserName"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Email" />
 
-              <TextInput type="text" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Password" />
 
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={succesdata}
+            >
+              {succesdata ? (
+                <>
+                  <Spinner size="sm" />
+
+                  <span className="pl-3">Loading....</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
 
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account</span>
-            <Link to='/sign-in' className='text-blue-500'>
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {errordata && (
+            <Alert className="mt-5 " color="failure">
+              {errordata}
+            </Alert>
+          )}
         </div>
       </div>
     </div>

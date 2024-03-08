8ea95e1f9457849a7dbc275d6f602f7a12e28abf
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-08 17:54:20 +0530

    sign inapi route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+// dotenv.config();
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -18,5 +20,32 @@ export const signup = async (req, res, next) => {
     res.json("Sign up successful");
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === " " || password === " ") {
+    return next(errorHandler(407, "Plese fill all details"));
+  }
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) {
+      return ext(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validuser._doc;
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error)
   }
 };
--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function signin() {
   return (
     <div>
-      sing-in
+      sign-in
     </div>
   )
 }

9e317b52c67f4d6800c996ce8db1ea25f7da15a1
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-08 19:37:45 +0530

    redux-toolkit

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,78 +1,71 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userslice.js";
+
 import {
   Alert,
   Button,
   Label,
-  RangeSlider,
   Spinner,
   TextInput,
 } from "flowbite-react";
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 
-export default function signin() {
+export default function SignIn() {
   const navigate = useNavigate();
-  const [formdata, setformdata] = useState({});
-  const [errordata, seterrordata] = useState(null);
-  const [succesdata, setsuccesdata] = useState(null);
-  const handlesubmit = async (e) => {
+  const [formData, setFormData] = useState({});
+  const { successData, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    if ( !formdata.email || !formdata.password) {
-      return seterrordata("please fill out all details");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("Please fill in all details"));
     }
     try {
-      setsuccesdata(true);
-      seterrordata(null);
+      dispatch(signInStart());
       const res = await fetch(`/api/auth/signin`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formdata),
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        return seterrordata(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setsuccesdata(false);
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      seterrordata(error.message);
-      setsuccesdata(false);
+      dispatch(signInFailure(error.message));
     }
   };
+
   const handleChange = (e) => {
-    setformdata({ ...formdata, [e.target.id]: e.target.value.trim() });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-  console.log(formdata);
+
   return (
-    <div className=" min-h-screen mt-20">
+    <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-7">
         <div className="flex-1 mt-20">
-          <Link to="/" className=" text-4xl font-bold dark:text-white">
+          <Link to="/" className="text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Tron
-            </span>
+            </span>{" "}
             Blog
           </Link>
-          <p className="mt-5 text-bold">
-            Welcome back Bro
-          </p>
+          <p className="mt-5 text-bold">Welcome back Bro</p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-3" onSubmit={handlesubmit}>
-            {/* <div>
-              <Label value="UserName" />
-
-              <TextInput
-                type="username"
-                placeholder="UserName"
-                id="username"
-                onChange={handleChange}
-              />
-            </div> */}
+          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="">
               <Label value="Email" />
-
               <TextInput
                 type="email"
                 placeholder="Email"
@@ -82,7 +75,6 @@ export default function signin() {
             </div>
             <div className="">
               <Label value="Password" />
-
               <TextInput
                 type="password"
                 placeholder="Password"
@@ -93,12 +85,11 @@ export default function signin() {
             <Button
               gradientDuoTone="purpleToPink"
               type="submit"
-              disabled={succesdata}
+              disabled={successData}
             >
-              {succesdata ? (
+              {successData ? (
                 <>
                   <Spinner size="sm" />
-
                   <span className="pl-3">Loading....</span>
                 </>
               ) : (
@@ -106,16 +97,16 @@ export default function signin() {
               )}
             </Button>
           </form>
-
           <div className="flex gap-2 text-sm mt-5">
-            <span>Don't Having account</span>
+            <span>Don't have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
           </div>
-          {errordata && (
-            <Alert className="mt-5 " color="failure">
-              {errordata}
+          {/* Added error message display */}
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
             </Alert>
           )}
         </div>

372cc3f6c9778c23c81ba9119f805b425e3af5ef
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-09 19:41:18 +0530

    redux persist state mangaemnet added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from "./user/userslice.js"
+
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userslice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage, // Correct usage of storage
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // Renamed variable to avoid conflict
+
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-})
+  reducer: persistedReducer, // Correct usage of persistedReducer
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);

315beb02cf9b5eeb1b523fd8ce8bb01ffe358632
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-10 19:21:40 +0530

    dashboard half complete

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+
 
 export default function dashboard() {
+   const location = useLocation()
+   const [tab,setTab]= useState('')
+   useEffect(()=>{
+
+    const searchParams = new URLSearchParams(location.search)
+    const tabfromurl = searchParams.get('tab')
+    console.log(tabfromurl)
+
+   },[location.search])
+  
   return (
-    <div>
-      dashboard
+    <div className=''>
+    
     </div>
   )
 }

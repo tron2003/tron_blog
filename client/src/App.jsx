e5e4883907687abddfcdd8b8be18ffb54844f15f
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-10 19:08:09 +0530

    make private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,26 +10,28 @@ import Signin from "./pages/signin";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
 import Project from "./pages/project";
-import Header from "./components/header"
-import Footer from "./components/footer"
+import Header from "./components/header";
+import Footer from "./components/footer";
+import PrivateRoute from "./components/privateroute";
 function App() {
   return (
     <BrowserRouter>
-
-    <Header/>
+      <Header />
       <Routes>
         <Route>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<Signup />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
 
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/projects" element={<Project />} />
         </Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }

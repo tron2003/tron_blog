dec7f8806769d0f8413ae4beaa2e8abe57b7405d
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-12 18:08:11 +0530

    update frontend working

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,23 +1,33 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-// dotenv.config();
-import jwt from "jsonwebtoken";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  if (!username || !email || !password || username === "" || email === "") {
-    next(errorHandler(400, "All fields are required"));
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
   }
 
-  // var salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  var hash = bcryptjs.hashSync(password, 10);
-  const newuser = new User({ username, email, password: hash });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
   try {
-    await newuser.save();
-
-    res.json("Sign up successful");
+    await newUser.save();
+    res.json('Signup successful');
   } catch (error) {
     next(error);
   }
@@ -25,23 +35,27 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-  if (!email || !password || email === " " || password === " ") {
-    return next(errorHandler(407, "Plese fill all details"));
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
   }
+
   try {
-    const validuser = await User.findOne({ email });
-    if (!validuser) {
-      return ext(errorHandler(404, "User not found"));
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
     }
-    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(400, "Invalid Password"));
+      return next(errorHandler(400, 'Invalid password'));
     }
-    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
-    const { password: pass, ...rest } = validuser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
     res
       .status(200)
-      .cookie("access_token", token, {
+      .cookie('access_token', token, {
         httpOnly: true,
       })
       .json(rest);
@@ -51,7 +65,7 @@ export const signin = async (req, res, next) => {
 };
 
 export const google = async (req, res, next) => {
-  const { email, name, googlePhoto } = req.body;
+  const { email, name, googlePhotoUrl } = req.body;
   try {
     const user = await User.findOne({ email });
     if (user) {
@@ -59,29 +73,29 @@ export const google = async (req, res, next) => {
       const { password, ...rest } = user._doc;
       res
         .status(200)
-        .cookie("access_token", token, {
+        .cookie('access_token', token, {
           httpOnly: true,
         })
         .json(rest);
     } else {
-      const generatedPassowrd =
+      const generatedPassword =
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
-      const hashedPassword = bcryptjs.hashSync(generatedPassowrd, 10);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          name.toLowerCase().split(" ").join("") +
-          Math.random().toString(9).slice(-3),
+          name.toLowerCase().split(' ').join('') +
+          Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
-        ProfilePicture: googlePhotoUrl,
+        profilePicture: googlePhotoUrl,
       });
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie("access_token", token, {
+        .cookie('access_token', token, {
           httpOnly: true,
         })
         .json(rest);
--- api/index.js
+++ api/index.js
@@ -15,7 +15,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 const app = express();
 
 app.use(express.json());
@@ -24,10 +23,8 @@ app.use(cookieParser());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
-
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';

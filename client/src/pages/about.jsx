d939e68f87a344e02b88d75802492abc8541090a
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-25 04:05:54 +0530

    about and project added

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function about() {
   return (
-    <div>
-      about
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl  mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About TronBlog
+          </h1>
+          <div className="text-md  text-teal-300 flex flex-col gap-6">
+            <p>
+              I’m Kanish, a BTech Computer Science student at PEC Chandigarh.
+              I’m passionate about Data Science and open-source contributions.
+              My goal is to excel in Data Science, using my skills in Advanced
+              Math and Python. I’m self-motivated to learn new concepts and
+              technologies at PEC Chandigarh to make meaningful contributions to
+              technology, including open-source projects
+            </p>{" "}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/project.jsx
+++ client/src/pages/project.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/calltoaction";
 
 export default function project() {
   return (
     <div>
-      project
+      <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-5">
+        <h1 className="text-3xl font-semibold">Projects</h1>
+        <p className="text-md  text-teal-300">
+          edfjwefjwefmnwemfwmefknmwekcwnencvwkemn;wvnwevnweknvoencvkwdnck
+        </p>
+        <CallToAction />
+      </div>
     </div>
-  )
+  );
 }

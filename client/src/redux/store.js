9fdffef4f06c2e37def82c142fe796b3f954ee3f
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-25 04:27:33 +0530

    ,

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice.js';
-import themeReducer from './theme/themeSlice.js';
-import { persistReducer, persistStore } from 'redux-persist/lib/index.js';
-import storage from 'redux-persist/lib/storage/index.js';
-
+import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -15,10 +14,13 @@ const persistConfig = {
   storage,
   version: 1,
 };
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
 export const persistor = persistStore(store);

110c53fd1b19671c514010c8ac964f713e8db5f7
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-25 04:35:03 +0530

    fd

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
-import themeReducer from './theme/themeSlice';
+import userReducer from './user/userSlice.js';
+import themeReducer from './theme/themeSlice.js';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
